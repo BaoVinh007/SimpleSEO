cc769bcd804253d0a9437d974084e6094ccf0d29
VinhNguyen <baovinh007@yahoo.com>
2014-11-04 09:33:46 -0800

    Output (text) of first  thread

--- class/RankingClass.php
+++ class/RankingClass.php
@@ -6,6 +6,10 @@
 		public $alexa_class;
 		public $page_rank;
 		public $pr_class;
+		public $domain;
+		public $url;
+		public $output;
+		
 		
 		public function __construct($domain,$url)
 		{
@@ -20,6 +24,76 @@
 			$this->alexa_class = $this->getAlexaRankCLass($this->alexa_rank);
 			$this->page_rank = $this->getPageRank($this->url);
 			$this->pr_class = $this->getPageRankClass($this->page_rank);
+			
+			
+			$ouput = '
+						<h2>Visitors</h2>	
+                        <div class="report_section" id="section_visitors">
+                            <div class="report_section_inner">
+								<div class="criterion">
+                                    <div class="criterion_value">
+                                        <h4>Traffic estimation</h4>                            
+	                                        <div>About '.$this->traffic.' Unique visitors/month	</div>
+											<div class="score_label"></div>
+											<a href="#" class="advice_toggle advice_open">Show advice</a>
+									</div>
+									<div class="criterion_info_advice">
+                                        <div class="criterion_advice">
+                                            <div>
+                                                <p>You can use several different tools to estimate web traffic: <a rel="nofollow" href="https://www.google.com/adplanner/#siteSearch?identifier='.$this->domain.'" target="_blank">Google&trade; Ad Planner</a>, <a rel="nofollow" href="http://trends.google.com/websites?q='.$this->domain.'" target="_blank">Google&trade; Trends</a>, and <a rel="nofollow" href="http://www.alexa.com/siteinfo/'.$this->domain.'" target="_blank">Alexa</a>.</p>
+                                                <p>Nevertheless, your analytics will provide the accurate traffic data.</p>																				 											</div>
+                                        </div>
+									</div>
+								</div>
+								<div class="'.$this->alexa_class.'">
+									<div class="criterion_value">
+										<h4>Alexa rank</h4>
+										<div>
+											<p>'.number_format($this->alexa_rank).'</p>	
+										</div>
+										<div class="score_label"></div>
+										<a href="#" class="advice_toggle advice_open">Show advice</a>
+									</div>
+									<div class="criterion_info_advice">
+										<div class="criterion_info">
+											<div class="criterion_importance level2">
+												<div class="icons"></div>
+												<div class="text">High impact</div>
+											</div>
+										</div>
+										<div class="criterion_advice">
+											<div>
+                                                <p>A low rank means that your website gets lots of visitors.</p><p>Your <a rel="nofollow" href="http://www.alexa.com/siteinfo/'.$this->domain.'" target="_blank">Alexa Rank</a> is a good estimate of worldwide traffic to your website, although it is <a rel="nofollow" href="http://blog.alexa.com/2009/08/alexa-101-anatomy-of-traffic-rank-graph.html" target="_blank">not 100% accurate</a>.</p><p>Reviewing the <a rel="nofollow" href="http://www.alexa.com/topsites/countries" target="_blank">most visited websites</a> by country can give you valuable insights.</p><p><a rel="nofollow" href="http://www.quantcast.com/'.$this->domain.'" target="_blank">Quantcast</a> provides similar services.</p>																			 											</div>
+										</div>
+									</div>
+								</div>
+                                                                
+								<div class="'.$this->pr_class.'">
+                                    <div class="criterion_value">
+                                    <h4>Page rank</h4>
+                                    <div>'.$this->page_rank.'</div>
+									<div class="score_label"></div>
+									<a href="#" class="advice_toggle advice_open">Show advice</a>
+								</div>
+								<div class="criterion_info_advice">
+									<div class="criterion_info">
+                                        <div class="criterion_importance level2">
+                                            <div class="icons"></div>
+                                            <div class="text">High impact</div>
+                                        </div>
+									</div>
+                                    <div class="criterion_advice">
+                                        <div>
+                                            <p>PageRank is a way to determine the relevance or importance of the websites/webpages and it is numerically represented from 0 to 10 largely based on number and quality of the backlinks.</p>														
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+			';
+			
+			$this->output = $ouput;
+			
 		}
 		
 		public function getTraffic($domain)
